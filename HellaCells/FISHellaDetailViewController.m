74312fafc203f07b707cdc954e09dd7b0b6722a6
chriswebb09 <chris.webb5249@gmail.com>
2016-06-21 17:19:56 -0400

    Code finished

---
+++ HellaCells/FISHellaDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  FISHellaDetailViewController.m
+//  HellaCells
+//
+//  Created by Christopher Webb-Orenstein on 6/21/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISHellaDetailViewController.h"
+
+@implementation FISHellaDetailViewController
+
+@end
